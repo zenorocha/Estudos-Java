cc8b3722a0b55d9ca6b861624cbfab259d33c3fb
Arruda <arruda@uniriotec.com>
2011-06-26 13:23:52 -0300

    tirado imports desncessecarios do ex5, feito ex6 e 7

--- src/br/blog/arruda/lista6/Exercicio05.java
+++ src/br/blog/arruda/lista6/Exercicio05.java
@@ -4,9 +4,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Exercicio05 {
 
