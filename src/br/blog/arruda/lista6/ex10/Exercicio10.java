fe22e1b775d1a140437c91f72b6b9f6c779652f1
Arruda <arruda@uniriotec.com>
2011-06-26 15:26:02 -0300

    exercicio 10 feito

--- src/br/blog/arruda/lista6/ex10/Exercicio10.java
+++ src/br/blog/arruda/lista6/ex10/Exercicio10.java
@@ -1,7 +1,13 @@
 package br.blog.arruda.lista6.ex10;
 
+import br.blog.arruda.lista6.ex10.DAO.ProdutoDAOSerBinaria;
+import br.blog.arruda.lista6.ex10.DAO.ProdutoDAOXML;
+import br.blog.arruda.lista6.ex10.DAO.ProdutoDAOXML;
 import br.blog.arruda.lista6.ex6.*;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Exercicio10 {
 
@@ -11,6 +17,31 @@ public class Exercicio10 {
             Produto p = new Produto("nome " + i + 1, i + 1, new Double(i + 1 * 100));
             listaProdutos.add(p);
         }
+
+        Scanner scanner = new Scanner(System.in);
+        Class c = null;
+        ProdutoDAOXML pDAOXml = new ProdutoDAOXML();
+        ProdutoDAOSerBinaria pDAOBin = new ProdutoDAOSerBinaria();
+
+        System.out.println("Escolha qual DAO deseja usar: ");
+        System.out.println(pDAOXml.getClass());
+        System.out.println(pDAOBin.getClass());
+        
+        try {
+            c = Class.forName(scanner.nextLine());
+        } catch (ClassNotFoundException ex) {
+            System.out.println("classe não existente.");
+            return;
+        }
+
+        if (c.isInstance(pDAOBin)) {
+            System.out.println(pDAOBin.armazena(listaProdutos));
+
+        } else if (c.isInstance(pDAOXml)) {
+            System.out.println(pDAOXml.armazena(listaProdutos));
+
+        }
+
 
 
 
