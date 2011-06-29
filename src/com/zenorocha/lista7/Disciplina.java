01a6191297590620c50d8bc9b8ff1357b4a9252e
Zeno Rocha <zno.rocha@gmail.com>
2011-06-29 18:40:54 -0300

    iniciando lista 8

--- src/com/zenorocha/lista7/Disciplina.java
+++ src/com/zenorocha/lista7/Disciplina.java
@@ -1,14 +1,31 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.zenorocha.lista7;
 
-/**
- *
- * @author zeno
- */
 public class Disciplina {
+
+    private String codigo;
+    private String nome;
+
+    public Disciplina() {}
+
+    public Disciplina(String codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
 }
